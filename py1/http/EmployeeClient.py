4a58d4dd5d3de72f8438adc6588b06b0e3cf4cfb
cosmi <cosmicy@outlook.com>
2016-06-30 23:03:21 +0800

    *分离出get和post

--- py1/http/EmployeeClient.py
+++ py1/http/EmployeeClient.py
@@ -2,15 +2,32 @@
 #coding=utf8
 
 import http.client
+import urllib
+from urllib import request
 
 httpClient = None
 
+
+
+def Get(httpClient):
+	httpClient.request('GET', '/api/employees?department=4')
+	return httpClient.getresponse()
+
+def Post(httpClient):
+	values = {"FirstName":"sss","LastName":"ddd","Department":"3"}
+	data = urllib.parse.urlencode(values)
+	data = data.encode('utf-8')
+
+	url = "http://localhost:5245/api/employees"
+
+	return request.urlopen(url,data)
+
+
 try:
     httpClient = http.client.HTTPConnection('localhost', 5245, timeout=30)
-    httpClient.request('GET', '/api/employees?department=4')
 
-    #response是HTTPResponse对象
-    response = httpClient.getresponse()
+    response = Post(httpClient)
+
     print(response.status)
     print(response.reason)
     print(response.read().decode("utf-8"))
@@ -19,3 +36,4 @@ except Exception as e:
 finally:
     if httpClient:
         httpClient.close()
+
